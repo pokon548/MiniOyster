59d1214b307b208cc3d0a2533998cec0c3c6c0a1
Bu Kun <65808665+pokon548@users.noreply.github.com>
2024-03-11 01:12:55 +0800

    minioyster: use flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, ... } @ inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      flake.nixosModules =
+        let
+          inherit (inputs.nixpkgs) lib;
+        in
+        {
+          minioyster = { };
+
+          prefstore-crick = { };
+          prefstore-partitio = { };
+          prefstore-hikari = { };
+          prefstore-ritsu = { };
+        };
+    };
+}
